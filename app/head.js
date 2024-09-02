ea319864c873fd5c2091775722a7f3cbf37e301a
JE2606 <juan.elias0626@gmail.com>
2024-09-01 20:44:04 -0400

    page complete

---
+++ app/head.js
@@ -0,0 +1,11 @@
+const Head = () => (
+  <>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charSet="UTF-8" />
+    <title>Metaversus</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+  </>
+);
+
+export default Head;
---
+++ sections/Hero.jsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+
+const Hero = () => (
+  <section className={`w-screen ${styles.yPaddings}`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`w-full grid justify-center`}
+    >
+      <div className="relative z-10">
+        <motion.h1 variants={textVariant(1.1)} className={`${styles.heroHeading} flex justify-center items-center`}>
+          Metaverse
+        </motion.h1>
+        <motion.div
+          variants={textVariant(1.2)}
+          className="flex justify-center items-center"
+        >
+          <h1 className={styles.heroHeading}>Ma</h1>
+          <div className={styles.heroDText} />
+          <h1 className={styles.heroHeading}>Ness</h1>
+        </motion.div>
+      </div>
+
+      <motion.div
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className={`w-screen relative md:-mt-[20px] -mt-[12px] ml-40`}
+      >
+        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
+
+        <img
+          src="/cover.png"
+          alt="hero_cover"
+          className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+        />
+
+        <a href="#explore">
+          <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[150px] relative z-10">
+            <img
+              src="/stamp.png"
+              alt="stamp"
+              className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
+            />
+          </div>
+        </a>
+      </motion.div>
+    </motion.div>
+  </section>
+);
+
+export default Hero;
